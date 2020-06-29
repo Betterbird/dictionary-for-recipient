6a2e65ed44fade5e18157102462d1b888c5f6bbf
Jorg Knobloch <none@none>
2020-06-29 22:09:38 +0200

    Initial commit after linting. Version 2.0.2

---
+++ 2.0.0-WE-Exp/background.js
@@ -0,0 +1,9 @@
+/* globals browser */
+
+var init = async () => {
+  browser.DictionaryForRecipient.addComposeWindowListener("dummy");
+};
+
+init();
+
+// console.log("DictionaryForRecipient: background");
